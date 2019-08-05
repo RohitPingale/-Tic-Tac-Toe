e19c5057018b76f0c9335e1d6a63bbc16eeadb07
EBπ <rohitpingale103@gmail.com>
2019-08-05 22:55:39 +0530

    making clean canvas

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="w3-sidebar w3-bar-block" style="width:25%">
+          <a href="#" class="w3-bar-item w3-button">Link 1</a>
+          <a href="#" class="w3-bar-item w3-button">Link 2</a>
+          <a href="#" class="w3-bar-item w3-button">Link 3</a>
+        </div>
+
+        <div style="margin-left:25%">
+        ... page content ...
+        </div> 
       </header>
     </div>
   );
