b92fc5408c815701d3b608f4561e7918518aac02
EBπ <rohitpingale103@gmail.com>
2019-08-25 00:43:28 +0530

    Home of our website

---
+++ src/home.js
@@ -0,0 +1,7 @@
+import React  from 'react';
+
+export const Home  = () => ( 
+<div>
+	<img src= {require("./UnderConstruction.png")} style = {{height: '500px', width: '500px', alignSelf: 'center'}} />
+</div>
+	)
