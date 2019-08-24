9238b1b763797d7a5618414dafd161898d5528ee
EBπ <rohitpingale103@gmail.com>
2019-08-25 00:44:09 +0530

    seperate page for info of our team members

---
+++ src/about.js
@@ -0,0 +1,7 @@
+import React  from 'react';
+
+export const Aboutus  = () => (
+<div> 
+	<img src= {require("./UnderConstruction.png")} style = {{height: '500px', width: '500px', alignSelf: 'center'}} />
+</div>
+	)
