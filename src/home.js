fd6a4f1aa2d4e72ae1c2855270f8f5a74bb90175
EBπ <rohitpingale103@gmail.com>
2019-08-26 23:23:29 +0530

    Homme page(nEED TO MAKE)

--- src/home.js
+++ src/home.js
@@ -1,7 +1,19 @@
 import React  from 'react';
+import { Card, Button, Image } from "react-bootstrap";
 
-export const Home  = () => ( 
-<div>
-	<img src= {require("./images/UnderConstruction.png")} style = {{height: '500px', width: '500px', alignSelf: 'center'}} />
-</div>
+export const Home  = () => (
+	<div> 
+		<Image src= {require("./images/UnderConstruction.png")}/>
+		<Card style={{ width: '18rem' }}>
+		  <Card.Img variant="top" src={require("./images/index.jpg")} />
+		  <Card.Body>
+		    <Card.Title>Tic-Tac-Toe</Card.Title>
+		    <Card.Text>
+		      Play Tictactoe
+		    </Card.Text>
+		    <Button variant="primary" href = '/tictactoe'>Go</Button>
+		  </Card.Body>
+		</Card>
+
+	</div>
 	)
