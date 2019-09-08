96ff2196c70f512de4b3bdd8b03fec2fde75b08c
EBπ <rohitpingale103@gmail.com>
2019-09-08 14:20:23 +0530

    server

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -6,9 +6,6 @@ import {Layout} from './components/Layout';
 export const TicTacToe = () => (
 	<Layout>
 			<TicNavbar/>
-			<style> body {
-				background-color: 
-			}
 			<canvas id = 'ttt'></canvas>
 			<p id ='message'>Welcom to TTT</p>
 			<script src = "components/ttt.js"></script>
